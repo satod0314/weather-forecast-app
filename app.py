3115d1849c7f41cb26bb06ba7789d7e56993e8cb
satod0314 <132069115+satod0314@users.noreply.github.com>
2025-05-23 14:58:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -160,5 +160,5 @@ def edit_script():
             'message': str(e)
         }), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
